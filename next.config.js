fd05862f4845d788fb06b1f15c1b02990d28643e
shahnawazfakir <shahnawazfakir@gmail.com>
2022-05-26 18:38:07 -0400

    updates

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// module.exports = {
-//   images: {
-//     domains: [
-//       'res.cloudinary.com',
-//       'avatars.githubusercontent.com',
-//       'imgur.com',
-//     ],
-//   },
-// };
-
 module.exports = {
   images: {
     loader: 'akamai',
